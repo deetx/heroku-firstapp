254a130884e728974b4c1208c83f2ad5ced55397
Joel Vornanen <joel.vornanen2@gmail.com>
2018-04-17 11:03:47 +0300

    travis test

---
+++ src/test/java/org/gradle/demo/HelloServletTest.java
@@ -0,0 +1,15 @@
+package org.gradle.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class HelloServletTest {
+    
+    @Test
+    public void dummyUnitTest() {
+        assertEquals(0, 1);
+    }
+    @Test
+    public void dummyUnitTest2() {
+        assertEquals(1, 1);
+    }
+}
