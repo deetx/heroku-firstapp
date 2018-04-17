fa5e18586f770f2109dc3e25c0d3fabe42594af5
Joel Vornanen <joel.vornanen2@gmail.com>
2018-04-17 11:45:30 +0300

    Integ test add

---
+++ src/integTest/java/dummyIntegrationTest.java
@@ -0,0 +1,11 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class dummyIntegrationTest {
+    
+    @Test
+    public void integrationTest2() {
+        assertEquals(0, 1);
+    }
+}
